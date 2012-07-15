42c042446ab213dc0b2fcc90836625763c0d4f96
Oleg NIzhnik <odomontois@gmail.com>
2012-07-16 03:06:20 +0400

    transfer to express

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var app = require('express').createServer();
 
 var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Bro!!!\n');
-}).listen(port, ipaddr);
+app.get("/",function(req,res){
+	res.send('Hello Bro!!!\n');
+})
 
-console.log("Server running at http://" + ipaddr + ":" + port + "/");
+app.listen(port)
+console.log("server is running at port " + port)
