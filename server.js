f96cb9034bbd2b065f656c445cacb8478f36f40f
Oleg NIzhnik <odomontois@gmail.com>
2012-07-16 03:42:24 +0400

    some another fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 app.get("/",function(req,res){
-	res.render('hello.jade');
+	res.render('hello');
 });
 
 app.set('views',__dirname +'/views');
