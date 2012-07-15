8a059ba7a60e3d81caabfca6a68e233453fdbd5d
Oleg NIzhnik <odomontois@gmail.com>
2012-07-16 03:40:24 +0400

    lolo

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 app.get("/",function(req,res){
-	res.send('Hello Bro!!!\n');
-})
+	res.render('hello.jade');
+});
+
+app.set('views',__dirname +'/views');
+app.set('view engine','jade');
 
 app.listen(port,ipaddr, function(){
   console.log("server listening on %d in %s mode", port, app.settings.env);
