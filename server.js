fd11d430f45bdde31db8dd4c17a86e4041feb4a2
Oleg NIzhnik <odomontois@gmail.com>
2012-07-16 02:32:22 +0400

    hello text changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello Bro!!!\n');
 }).listen(port, ipaddr);
 
 console.log("Server running at http://" + ipaddr + ":" + port + "/");
