197b4386bd1845426a5331f4fa19f28b6c1e45ce
Oleg NIzhnik <odomontois@gmail.com>
2012-07-17 01:39:08 +0400

    dfsdfsdf

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-var app = require('express').createServer();
+var app = require('./app/index.js');
+var express = require('express');
 
 var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-app.get("/",function(req,res){
-	res.render('hello');
-});
-
 app.set('views',__dirname +'/views');
 app.set('view engine','jade');
+
+app.use("/styles", express.static(__dirname + '/styles'));
 
 app.listen(port,ipaddr, function(){
   console.log("server listening on %d in %s mode", port, app.settings.env);
